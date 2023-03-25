866b6bc3bfe27f50d896de31ea9cdb7b2cb8f9a6
YOUR NAME HERE <YOUR@EMAIL.com>
2023-03-24 23:46:10 -0700

    milestone4 wing

---
+++ analysis/code/project_functions1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def load_and_process(url_or_path_to_csv_file):
+    dfx = (
+        pd.read_csv('../data/raw/listings.csv')
+        .sort_values("minimum_nights", ascending=False)
+        .rename(columns={"minimum_nights": "minnights"})
+        .query("minnights > 1")
+        .reset_index(drop=True))
+
+    dfy = (dfx)
+    
+    return dfy
+
