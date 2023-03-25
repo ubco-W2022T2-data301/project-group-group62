a396db2fb7e75b564777317167b61d5190138331
YOUR NAME HERE <YOUR@EMAIL.com>
2023-03-25 00:24:16 -0700

    milestone wing

--- analysis/code/project_functions1.py
+++ analysis/code/project_functions1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_and_process(url_or_path_to_csv_file):
     dfx = (
-        pd.read_csv('../data/raw/listings.csv')
+        pd.read_csv('../data/processed/wingfixed.csv')
         .sort_values("minimum_nights", ascending=False)
         .rename(columns={"minimum_nights": "minnights"})
         .query("minnights > 1")
