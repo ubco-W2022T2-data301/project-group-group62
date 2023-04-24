91d39caf6070f34ef7930d827e2043f962a13af6
YOUR NAME HERE <YOUR@EMAIL.com>
2023-04-23 22:34:01 -0700

    updated

--- analysis/codes/project_function_WingFan.py
+++ analysis/codes/project_function_WingFan.py
@@ -3,14 +3,23 @@ import numpy as np
 
 def load_and_process(url_or_path_to_csv_file):
     dfx = (
-        pd.read_csv('../data/processed/wingfixed.csv')
+        pd.read_csv("../data/processed/wingfixed.csv")
         .sort_values("minimum_nights", ascending=False)
         .rename(columns={"minimum_nights": "minnights"})
         .query("minnights > 1")
-        .reset_index(drop=True))
-        .dropna())
+        .reset_index(drop=True)
+        .dropna()
+    ) 
 
-    dfy = (dfx)
-    
+    dfy = (
+        dfx
+        .assign(
+            rental_type=lambda x: pd.cut(x['minnights'], bins=5, labels=['short vaca', 'long vaca', 'short accomodation', 'long accomodation', 'long term rentals']),
+            has_wifi=lambda x:x['amenities'].apply(lambda x : True if 'wifi' in x else False),
+            long_term_available=lambda x:x['amenities'].apply(lambda x: True if 'Long term stays allowed' in x else False)
+        ))
+
+        
+
     return dfy
 
